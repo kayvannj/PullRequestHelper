774f320ed5d53c55d7aaa9ec18145468a4b682dd
Kayvan <kayvan.najafzadeh@gmail.com>
2016-04-29 13:34:20 -0700

    fixing bugs and adding installer

--- install.sh
+++ install.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 cp prh.py /usr/local/bin/prh
 chmod +x /usr/local/bin/prh
-<<<<<<< HEAD
 username=$1
 token=$2
-echo "github.com:
-- user: $username
-  oauth_token: $token
-  protocol: https" > ~/.config/hub
-=======
-hub --version
->>>>>>> master
+if [[ ! -z $1 ]]; then
+  echo "github.com:
+  - user: $username
+    oauth_token: $token
+    protocol: https" > ~/.config/hub
+  else
+    echo "
+------------------------------------
+You have not provided username and token,
+Make sure ~/.config/hub contains your credentials.
+
+You can use:  install.sh <username> <token>
+to create the file with correct credentials
+------------------------------------
+
+prh install was succesful
+"
+fi
--- prh.py
+++ prh.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 import subprocess
 import sys
+# import subprocess.CalledProcessError
 
 # user is on version branch
 # running prh -b "branch" -a <file 1> <file 2> <file 3> ...
 USEAGE = """
+    You can use prh in two main ways:
+    1) when you are in a parent branch and want to make a PR that contains your changes to the branch you are at
     prh -b <child_branch_name> [-a <file1_path> <file2_path> ...]
+    2) when you are in a child branch and want to just make a PR to a specific parent branch
     prh -upto <parent_branch_name>
-
 """
 DEFAULT_COMMIT_MESSAGE = "Commit"
 DEFAULT_BRANCH_NAME = "prh_branch"
@@ -16,21 +19,22 @@ DEFAULT_PR_BODY = "@doximitystevenlee CR please\n"
 debug_is_on = 0
 verbose_is_on = 0
 
-
 def run_command(command, output=0):
     if debug_is_on:
         print_command(command)
-    elif verbose_is_on:
-        print command
-        if output:
-            return subprocess.check_output(command)
-        else:
-            return subprocess.check_call(command)
     else:
-        if output:
-            return subprocess.check_output(command)
-        else:
-            return subprocess.check_call(command)
+        if verbose_is_on:
+            print command
+
+        try:
+            if output:
+                return subprocess.check_output(command)
+            else:
+                return subprocess.check_call(command)
+        except subprocess.CalledProcessError as e:
+            return e.output
+
+
 
 
 def print_command(command):
@@ -39,7 +43,10 @@ def print_command(command):
 
 def create_branch(branch_name):
     command = ["git", "checkout", "-b", branch_name]
-    run_command(command)
+    res = run_command(command)
+    if res and "already exists" in res:
+        print branch_name+" branch already exists"
+        checkout(branch_name)
 
 
 def checkout(branch_name):
@@ -49,12 +56,12 @@ def checkout(branch_name):
 
 def add_files(file_paths):
     command = ["git", "add"] + file_paths
-    return run_command(command)
+    return run_command(command,1)
 
 
 def add_all():
     command = ["git", "add", "-A"]
-    return run_command(command)
+    return run_command(command,1)
 
 
 def commit(commit_message=DEFAULT_COMMIT_MESSAGE):
@@ -66,7 +73,7 @@ def commit(commit_message=DEFAULT_COMMIT_MESSAGE):
 
 def push(branch_name):
     command = ["git", "push", "--set-upstream", "origin", branch_name]
-    run_command(command)
+    return not run_command(command,1)
 
 
 def create_pull_request(main_branch, pr_title=DEFAULT_PR_TITLE, pr_body=DEFAULT_PR_BODY):
@@ -82,11 +89,14 @@ def create_pull_request(main_branch, pr_title=DEFAULT_PR_TITLE, pr_body=DEFAULT_
 
 def get_current_branch():
     branches = subprocess.check_output(["git", "branch"])
-    for b in branches.split("\n"):
-        if b[0] == "*":
-            current_branch = b.split(" ")[1]
-            break
-    return current_branch
+    try:
+        for b in branches.split("\n"):
+            if b[0] == "*":
+                current_branch = b.split(" ")[1]
+                break
+        return current_branch
+    except:
+        return "master"
 
 
 def get_submodule_name():
@@ -115,13 +125,15 @@ def cd(path):
 
 def add_changes(file_paths, is_add_all):
     if is_add_all:
-        res = add_all()
+        error = add_all()
     elif file_paths:
-        res = add_files(file_paths)
+        error = add_files(file_paths)
 
-    if res:
-        print("No files to be added! Use -a <file path> to add files")
-        return -1
+    if error:
+        print("No files to be added!")
+        return 0
+    else:
+        return 1
 
 
 def main():
@@ -135,6 +147,10 @@ def main():
     submodule = 0
     current_branch = get_current_branch()
     is_add_all = False
+    if "--version" in sys.argv:
+        print "1.0.1"
+        return
+
     if "-debug" in sys.argv or "-d" in sys.argv:
         global debug_is_on
         debug_is_on = 1
@@ -164,7 +180,7 @@ def main():
         submodule = 1
 
     if "-m" in sys.argv:
-        commit_message = sys.argv[sys.argv.index("-m") + 1:]
+        commit_message = str(sys.argv[sys.argv.index("-m") + 1])
 
     if submodule:
         cd(get_submodule_name())
@@ -176,20 +192,21 @@ def main():
     elif parent_branch:
         target_branch_name = parent_branch
     else:
-        print "Use -h to see the usage"
+        print USEAGE
         return
 
     # add and commit changes
     add_changes(file_paths, is_add_all)
     commit(commit_message)
 
-    push(get_current_branch())
+    # push the branch
+    push(current_branch)
     pr_url = create_pull_request(target_branch_name, pr_title, pr_body)
     if pr_url[:4] == "http":
         print(pr_url)
         launch_browser(pr_url)
     else:
-        print(pr_url)
+        print("\n"+pr_url+"\n")
 
     checkout(current_branch)
 
