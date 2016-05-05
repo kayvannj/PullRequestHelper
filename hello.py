9a1a22b1ae463f853089f96a5edfe7d3028533ec
Kayvan <kayvan.najafzadeh@gmail.com>
2016-05-05 14:20:31 -0700

    Commit

--- hello.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-__author__ = 'kayvan'
-
-popen = subprocess.Popen("git status | head -n 1 | cut -f 3 -d ' '", stdin=subprocess.PIPE, shell=True,
-                         stdout=subprocess.PIPE)
-print popen.communicate()[0]
-# , "|", "head", "-n", "1", "|", "cut", "-f", "3", "-d", "\' \'"
