2839123f5752a69cf6222f843bfab4b1e9453cfe
Kayvan <kayvan.najafzadeh@gmail.com>
2017-02-24 08:36:58 -0800

    parsing multiple story links

--- prh_tests.py
+++ prh_tests.py
@@ -1,5 +1,6 @@
 import os
 import unittest
+
 import prh
 
 
@@ -15,3 +16,15 @@ class PrhTests(unittest.TestCase):
         self.assertTrue(os.path.isfile(new_config_path), "new config doesn't exists after migration")
         config_file = prh.read_from_config_file(file_path=new_config_path)
         self.assertEqual(config_file["test1_param1"], 'test1 Value1', "the values didn't carry over to new config")
+
+    def test_pr_template_append(self):
+        pass
+
+    def test_multiple_link_in_commit_message(self):
+        cm, full_url, story_ids = prh.parse_commit_message(
+            "this ishttps://www.pivotaltracker.com/story/show/140176051 https://www.pivotaltracker.com/story/show/139604723a test",
+            [], [])
+        self.assertEqual(cm, "this is a test")
+        self.assertEqual(full_url, ["https://www.pivotaltracker.com/story/show/140176051",
+                                    "https://www.pivotaltracker.com/story/show/139604723"])
+        self.assertEqual(story_ids, ["140176051", "139604723"])
