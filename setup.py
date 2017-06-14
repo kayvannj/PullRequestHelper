a4f2aec723c52919d6d34bb39c3c21097089ae78
Kayvan <kayvan.najafzadeh@gmail.com>
2017-06-14 12:48:05 -0700

    adding setup file, fixes #63

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PullRequestHelper',
+    version='2.3.0',
+    packages=[''],
+    url='github.com/kayvannj/PullRequestHelper',
+    license='Apache License, Version 2.0',
+    author='Kayvan Najafzadeh',
+    author_email='kayvannj@gmail.com',
+    description='A tool to help creating PullRequests on Github easier and automate some of the steps'
+)
